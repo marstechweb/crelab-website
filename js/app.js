96923e9befb7c78b7212ca57bd7d484c51f1736e
arohablue <amit42308@gmail.com>
2021-09-06 00:34:43 +0530

    added crasoul

--- js/app.js
+++ js/app.js
@@ -1,43 +1,64 @@
-(function($, document, window){
-	
-	$(document).ready(function(){
+(function ($, document, window) {
+  $(document).ready(function () {
+    if ($(".brands_slider").length) {
+      var brandsSlider = $(".brands_slider");
 
-		// Cloning main navigation for mobile menu
-		$(".mobile-navigation").append($(".main-navigation .menu").clone());
+      brandsSlider.owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        nav: false,
+        dots: false,
+        autoWidth: true,
+        items: 8,
+        margin: 42,
+      });
 
-		// Mobile menu toggle 
-		$(".menu-toggle").click(function(){
-			$(".mobile-navigation").slideToggle();
-		});
+      if ($(".brands_prev").length) {
+        var prev = $(".brands_prev");
+        prev.on("click", function () {
+          brandsSlider.trigger("prev.owl.carousel");
+        });
+      }
 
-		$(".hero").flexslider({
-			directionNav: false,
-			controlNav: true,
-		});
+      if ($(".brands_next").length) {
+        var next = $(".brands_next");
+        next.on("click", function () {
+          brandsSlider.trigger("next.owl.carousel");
+        });
+      }
+    }
+    // Cloning main navigation for mobile menu
+    $(".mobile-navigation").append($(".main-navigation .menu").clone());
 
-		var map = $(".map");
-		var latitude = map.data("latitude");
-		var longitude = map.data("longitude");
-		if( map.length ){
-			
-			map.gmap3({
-				map:{
-					options:{
-						center: [latitude,longitude],
-						zoom: 15,
-						scrollwheel: false
-					}
-				},
-				marker:{
-					latLng: [latitude,longitude],
-				}
-			});
-			
-		}
-	});
+    // Mobile menu toggle
+    $(".menu-toggle").click(function () {
+      $(".mobile-navigation").slideToggle();
+    });
 
-	$(window).load(function(){
+    $(".hero").flexslider({
+      directionNav: false,
+      controlNav: true,
+    });
 
-	});
+    var map = $(".map");
+    var latitude = map.data("latitude");
+    var longitude = map.data("longitude");
+    if (map.length) {
+      map.gmap3({
+        map: {
+          options: {
+            center: [latitude, longitude],
+            zoom: 15,
+            scrollwheel: false,
+          },
+        },
+        marker: {
+          latLng: [latitude, longitude],
+        },
+      });
+    }
+  });
 
+  $(window).load(function () {});
 })(jQuery, document, window);
